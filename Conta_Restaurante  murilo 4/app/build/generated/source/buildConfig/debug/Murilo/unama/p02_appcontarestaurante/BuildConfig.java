cd38be263f6a86ff98f58241f259f2f074d93d4b
MuriloSinimbu <42191498+MuriloSinimbu@users.noreply.github.com>
2019-03-30 19:21:55 -0300

    Add files via upload

---
+++ Conta_Restaurante  murilo 4/app/build/generated/source/buildConfig/debug/Murilo/unama/p02_appcontarestaurante/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package murilo.unama.p02_appcontarestaurante;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.unama.p02_appcontarestaurante";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ Conta_Restaurante  murilo 4/app/src/main/java/murilo/unama/p02_appcontarestaurante/MainActivity.java
@@ -0,0 +1,35 @@
+﻿package Luiz.unama.p02_appcontarestaurante;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // 1. declarar os componentes dinâmicos:
+    EditText editValor;
+    TextView txtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 2. Integração com o XML:
+        editValor = findViewById(R.id.edit_valor);
+        txtResultado = findViewById(R.id.txt_resultado);
+    } // fim do onCreate
+
+    // 3. método para calcular o resultado:
+    public void calcResultado(View v) {
+        // 4. pegar valor digitado:
+        String valorDigitado = editValor.getText().toString();
+        // 5. converter String para double:
+        double valor = Double.parseDouble(valorDigitado);
+        // 6. realizar o cálculo:
+        double resultado = valor * 1.1;
+        // 7. atribuir o resultado na tela:
+        txtResultado.setText("Conta com 10%: R$ "+resultado);
+    } // fim do calcResultado
+
+} // fim da classe
